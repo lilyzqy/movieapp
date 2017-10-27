f1d9463c8b7979612a536762da0d2f4fc59c7c83
Lily Yang <zq.yang.lily@gmail.com>
2017-10-27 16:43:39 -0700

    create band views

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,2 +1,40 @@
 class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+    render :index
+  end
+
+  def create
+    @band = Band.new(name:params[:name])
+    if @band.save
+      redirect_to :show
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :new
+    end
+  end
+
+  def new
+    render :new
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    @band = Band.find(id:params[:id])
+    if @band.update(name: params[:name])
+      redirect_to :show
+  end
+
+  def show
+    render :show
+  end
+
+  def destroy
+    @band = Band.find(id:params[:id])
+    @band.destroy
+    redirect_to :index
+  end
 end
