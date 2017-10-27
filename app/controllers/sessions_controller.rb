b8825d33e87e43a700b9bd2f3fcf211f5b8c8801
Lily Yang <zq.yang.lily@gmail.com>
2017-10-27 14:05:34 -0700

    user session controller done, view next

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  before_action :require_logged_in, only: [:destroy]
+  before_action :require_logged_out, only: [:new, :create]
+
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:email],params[:user][:password])
+    if @user.nil?
+      flash.now[:errors] = "Invalide email/password"
+    else
+      login!(@user)
+      redirect_to user_url
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
+end
